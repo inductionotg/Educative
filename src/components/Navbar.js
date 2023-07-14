63e38f8c55b982d3b85f01b348380322cb1737bf
inductionotg <ritesh2000.sinha@gmail.com>
2023-07-14 13:09:23 +0530

    Added changes to routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Navbar.css";
 import logo from '../assests/logo/images.png'
+import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
     <nav className="navbar-container">
       <div>
         <img src={logo} alt="logo" className="navbar-logo" />
-        <span>educative</span>
+        <Link to='/'><span>educative</span></Link>
       </div>
       <div className="navbar-items">
         <ul className="menu-item">
